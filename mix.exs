3b576225b912d44fb160a3ed4fd0bcb16ac8187a
Dylan Clements <dylan.clements99@gmail.com>
2023-06-07 11:40:45 +1000

    bump deps

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ExponentServerSdk.Mixfile do
   def project do
     [
       app: :exponent_server_sdk,
-      version: "0.2.0",
-      elixir: "~> 1.7.2",
+      version: "0.3.0",
+      elixir: "~> 1.13.4",
       name: "ExponentServerSdk",
       description: "Exponent Push Notification API library for Elixir",
       source_url: "https://github.com/rdrop/exponent-server-sdk-elixir",
@@ -21,13 +21,13 @@ defmodule ExponentServerSdk.Mixfile do
 
   defp deps do
     [
-      {:httpoison, ">= 1.2.0"},
-      {:poison, "~> 3.1.0"},
+      {:httpoison, "~> 1.8"},
+      {:poison, "~> 3.1"},
       {:dialyze, "~> 0.2.1", only: [:dev, :test]},
       {:credo, "~> 0.10.0", only: [:dev, :test]},
       {:mock, "~> 0.3.2", only: :test},
       {:ex_doc, ">= 0.0.0", only: [:dev, :test]},
-      {:inch_ex, ">= 0.0.0", only: [:dev, :test]}
+      {:inch_ex, ">= 0.0.0", only: [:dev, :test]},
     ]
   end
 
